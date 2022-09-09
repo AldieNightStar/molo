9eeb6c4a3288845e6119e4cb61f67ee0be4a4833
AldieNightStar <Aldie.NightStar@gmail.com>
2022-09-09 10:13:54 +0200

    Moloc

---
+++ moloc.py
@@ -0,0 +1,14 @@
+#!/bin/env python
+from molo import compile
+
+if __name__ == "__main__":
+    def _f():
+        import sys
+        args = sys.argv[1:]
+        if len(args) < 1:
+            print("Please enter file name")
+            return
+        filename = args[0]
+        chapters, creg = compile(filename)
+        print(chapters)
+    _f()
